44e175d2faec69b53738f410388bd23c543fb68d
michaelchen22244 <michael.chen@bzdlab.com>
2025-09-28 21:50:28 +0800

    Use multiply function in index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function greet() {
   console.log('Hello World');
   console.log('Welcome!');
   console.log('2 + 3 =', utils.add(2, 3));
+  console.log('5 * 4 =', utils.multiply(5, 4));
 }
 
 greet();
